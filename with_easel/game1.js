910aa5cd8a08afc713b2a9e6cb562b692cc95d87
Craven <wcraven@viasat.com>
2015-07-19 09:22:32 -0700

    Creating 1 player game

---
+++ with_easel/game1.js
@@ -0,0 +1,149 @@
+
+/*
+function Actor(color, w, h) {
+    this.pixelsPerSecond = 100;
+
+    this.width = w;
+    this.height = h;
+    this.graphics.beginFill(color).drawRect(0,0,w,h);
+
+    this.movementCalculation = function(delta) {
+        return (delta)/1000*this.pixelsPerSecond;
+    }
+    this.moveUp = function(delta) {
+        if(this.y-this.movementCalculation(delta) > 0)
+            this.y -= this.movementCalculation(delta);
+    }
+    this.moveDown = function(delta) {
+        if(this.y+this.movementCalculation(delta)+this.height < 500)
+            this.y += this.movementCalculation(delta);
+    }
+    this.moveLeft = function(delta) {
+        if(this.x-this.movementCalculation(delta)> 0)
+            this.x -= this.movementCalculation(delta);
+    }
+    this.moveRight = function(delta) {
+        if(this.x+this.movementCalculation(delta)+this.height < 500)
+            this.x += this.movementCalculation(delta);
+    }
+}
+
+
+Actor.prototype = new createjs.Shape();
+*/
+
+//window.Actor = Actor;
+
+pixelsPerSecond = 100;
+movementCalculation = function(delta) {
+    return (delta)/1000*pixelsPerSecond;
+}
+moveUp = function(s, delta) {
+    if(s.y-movementCalculation(delta) > 0)
+        s.y -= movementCalculation(delta);
+}
+moveDown = function(s, delta) {
+    if(s.y+ movementCalculation(delta)+s.height < 500)
+        s.y += movementCalculation(delta);
+}
+moveLeft = function(s, delta) {
+    if(s.x-movementCalculation(delta)> 0)
+        s.x -= movementCalculation(delta);
+}
+moveRight = function(s,delta) {
+    if(s.x+movementCalculation(delta)+s.height < 500)
+        s.x += movementCalculation(delta);
+}
+
+function checkCollisionWithCircle(rect, circle){
+    var distX = Math.abs(circle.x - rect.x-rect.width/2);
+    var distY = Math.abs(circle.y - rect.y-rect.height/2);
+
+    if (distX > (rect.width/2 + circle.r)) { return false; }
+    if (distY > (rect.height/2 + circle.r)) { return false; }
+
+    if (distX <= (rect.width/2)) { return true; }
+    if (distY <= (rect.height/2)) { return true; }
+
+    var dx=distX-rect.width/2;
+    var dy=distY-rect.height/2;
+    return (dx*dx+dy*dy<=(circle.r*circle.r));
+}
+
+function init() {
+
+    var stage = new createjs.Stage("mainCanvas");
+    var myActor = stage.addChild(new createjs.Shape());
+    myActor.width=50
+    myActor.height=50
+    myActor.graphics.beginFill("#000000").drawRect(0,0,myActor.width,myActor.height);
+    myActor.x = 100;
+    myActor.y = 100;
+    score = 0
+
+    timer = 30
+
+    target = stage.addChild(new createjs.Shape());
+    target.r = 45;
+    target.graphics.beginFill("red").drawCircle(0,0,target.r)
+            .beginFill("white").drawCircle(0,0,30)
+            .beginFill("red").drawCircle(0,0,15);
+    target.x = 300;
+    target.y = 380;
+
+
+    var up = false;
+    var down = false;
+    var left = false;
+    var right = false;
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener("tick", stage);
+    createjs.Ticker.addEventListener("tick", tick);
+
+    setInterval(function (){
+        if(timer>0){
+            timer -= 1
+        }
+    }, 1000);
+
+    function tick(event) {
+
+        if(timer==0){
+            $("#score").remove()
+            $("#Body").html(header + "<h4>Final Score: " + score + "</h4>")
+            return
+        }
+        if (key.isPressed('up')) {
+            moveUp(myActor, event.delta);
+        }
+        if (key.isPressed('down')) {
+            moveDown(myActor,event.delta);
+        }
+        if (key.isPressed('left')) {
+            moveLeft(myActor,event.delta);
+        }
+        if (key.isPressed('right')) {
+            moveRight(myActor,event.delta);
+        }
+
+        if (checkCollisionWithCircle(myActor, target)){
+            console.log("hit")
+            empty = false
+            while (empty == false){
+                target.x = Math.random()*500
+                target.y = Math.random()*500
+                if ((! checkCollisionWithCircle(myActor, target))){
+                    empty = true
+                }
+            }
+            score += 1
+        }
+
+    $("#score").html("Score: " + score + "<br> Time Left: " + timer);
+
+    }
+}
+
+
+
+
